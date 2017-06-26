38f0421e343a9671227b85d413675e5d58648c3b
Karan171996 <krnsngh38@gmail.com>
2017-06-26 17:55:35 +0530

    clock added to github-battle

--- github-battle/app/components/Home.js
+++ github-battle/app/components/Home.js
@@ -1,17 +1,23 @@
 var React = require('react');
 var Link = require('react-router-dom').Link;
+var Time = require('./Clock');
+
+
 class Home extends React.Component {
 	render(){
+        
          return(
          	 <div className='home-container'>
                 <h1>Github Battle :Battle your friends ... and stuff.</h1>
-             
+                 
                 <Link className='button' to='/battle'>
                 Battle
                 </Link>
-             
+              <Time />
              </div>
          	 )
-	}
+
+        }
 }
+
 module.exports = Home
